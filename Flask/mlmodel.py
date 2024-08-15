b8cd32c3f1f70ccf71563e21bbc88e711a900aac
absolshiny <absol.1994@gmail.com>
2024-08-15 15:13:27 -0600

    fix naming

--- Flask/mlmodel.py
+++ Flask/mlmodel.py
@@ -6,8 +6,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 from .preprocess import pre
 warnings.filterwarnings("ignore")
-def predict(newPassengers):
-    numberNewPassengers = len(newPassengers)
+def predict(new_passengers):
+    number_new_passengers = len(new_passengers)
     with open('Titanic_pipeline_model_nopre.pkl', 'rb') as f:
         loaded_pipeline = pickle.load(f)
 
@@ -20,18 +20,18 @@ def predict(newPassengers):
         ('classifier', loaded_pipeline.named_steps['classifier'])
     ])
     
-    newPassengers = pd.DataFrame(newPassengers)
-    newPassengers['PassengerId'] = range(1310, 1310 + len(newPassengers))
-    newPassengers['Fare'] = pd.to_numeric(newPassengers['Fare'], errors='coerce')
-    newPassengers['Age'] = pd.to_numeric(newPassengers['Age'], errors='coerce')
-    newTest = pd.concat([test, newPassengers], ignore_index=True)
+    new_passengers_df = pd.DataFrame(new_passengers)
+    new_passengers_df['PassengerId'] = range(1310, 1310 + len(new_passengers_df))
+    new_passengers_df['Fare'] = pd.to_numeric(new_passengers_df['Fare'], errors='coerce')
+    new_passengers_df['Age'] = pd.to_numeric(new_passengers_df['Age'], errors='coerce')
+    new_test = pd.concat([test, new_passengers_df], ignore_index=True)
 
-    preds = new_pipeline.predict(newTest)
-    ids = newTest['PassengerId']
+    preds = new_pipeline.predict(new_test)
+    ids = new_test['PassengerId']
 
     PredictionDF = pd.DataFrame({'PassengerId' : ids, 'Survived' : preds})
 
-    last_five = PredictionDF.tail(numberNewPassengers)
+    last_five = PredictionDF.tail(number_new_passengers)
     last_five_dict = last_five.to_dict('records')
     json_data = json.dumps(last_five_dict)
     return json_data
