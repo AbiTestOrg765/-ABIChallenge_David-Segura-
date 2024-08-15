ed794d70aa685f18420fccd092e0f143cb9a8f2b
DavidSmarttracker <david@smarttracker.health>
2024-08-15 13:28:13 -0600

    init file

--- Flask/__init__.py
+++ Flask/__init__.py
@@ -1,3 +1,15 @@
-import os
+from flask import Flask, Blueprint
+from flask_cors import CORS
+import logging
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # Enable CORS for development
 
-from flask import Flask
+    # Import blueprints or other components here
+    from .routes import routes_blueprint
+
+    app.register_blueprint(routes_blueprint)
+
+    return app
+
+create_app()
