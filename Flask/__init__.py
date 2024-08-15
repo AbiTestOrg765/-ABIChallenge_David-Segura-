897566c01310a8ddfd2d874524b71055d0458823
absolshiny <absol.1994@gmail.com>
2024-08-15 16:16:18 -0600

    CSRFPROTECT

--- Flask/__init__.py
+++ Flask/__init__.py
@@ -3,9 +3,11 @@ from flask_cors import CORS
 import logging
 def create_app():
     app = Flask(__name__)
+    csrf = CSRFProtect()
+    csrf.init_app(app)
     CORS(app)  # Enable CORS for development
 
-    # Import blueprints or other components here
+    # Import blueprints
     from .routes import routes_blueprint
 
     app.register_blueprint(routes_blueprint)
