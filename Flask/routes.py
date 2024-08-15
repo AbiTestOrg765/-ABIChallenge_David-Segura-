30a555190ba9682dcd5a5407ed881e9a98d5d74b
absolshiny <absol.1994@gmail.com>
2024-08-15 15:13:34 -0600

    fix naming

--- Flask/routes.py
+++ Flask/routes.py
@@ -6,10 +6,10 @@ routes_blueprint = Blueprint('routes', __name__)
 @routes_blueprint.route('/newuser', methods=['POST'])
 def create_user_route():
     data = request.get_json()
-    userName = data.get('userName')
+    user_name = data.get('userName')
     password = data.get('password')
-    newUserName = data.get('newUserName')
-    newPassword = data.get('newPassword')
+    new_user_name = data.get('newUserName')
+    new_password = data.get('newPassword')
     userid = create_user(userName, password, newUserName, newPassword)
     if userid:
         response = {'message': 'User created successfully'}
@@ -22,29 +22,29 @@ def create_user_route():
 @routes_blueprint.route('/predict_group_survival', methods=['GET'])
 def predictsurvival():
     data = request.get_json()
-    userName = data.get('userName')
+    user_name = data.get('userName')
     password = data.get('password')
     passengers = data.get('passengers')
-    userId = authenticate_user(userName, password)
-    if not userId:
+    user_id = authenticate_user(user_name, password)
+    if not user_id:
         response = jsonify({'message': 'Invalid credentials'}), 401
         return response
     response = predict(passengers)
-    create_log(userId, "GET", data, response)
+    create_log(user_id, "GET", data, response)
     return jsonify(response), 201
 
 @routes_blueprint.route('/predict_individual_survival', methods=['GET'])
 def predictsurvival():
     data = request.get_json()
-    userName = data.get('userName')
+    user_name = data.get('userName')
     password = data.get('password')
     passenger = data.get('passengers')
-    userId = authenticate_user(userName, password)
-    if not userId:
+    user_id = authenticate_user(user_name, password)
+    if not user_id:
         response = jsonify({'message': 'Invalid credentials'}), 401
         return response
     response = predict([passenger])
-    create_log(userId, "GET", data, response)
+    create_log(user_id, "GET", data, response)
     return jsonify(response), 201
 
 @routes_blueprint.route('/health')
